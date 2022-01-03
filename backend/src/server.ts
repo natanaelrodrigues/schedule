e791b7b67d6002c7f66243665ba5763e9d026e59
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2022-01-03 11:43:09 -0300

    Criação da Variável de ambiente

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import  express, { Response, Request } from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
 import maintRoutes from './routes/index'
+
+dotenv.config();
 
 const  server = express();
 
 const hostname: string = "127.0.0.1";
-const port: number = 8081;
 
 server.use(express.static(path.join(__dirname,'../public')));
 
@@ -16,8 +18,8 @@ server.use((req: Request, res: Response) => {
   res.status(404).send('<h1>Página não encontrada.</h1>')
 })
 
-server.listen(port, function () {
+server.listen(process.env.PORT, function () {
     console.log(
-      `App listening on port ${port} in link http://${hostname}:${port}`
+      `App listening on port ${process.env.PORT} in link http://${hostname}:${process.env.PORT}`
     );
   });
